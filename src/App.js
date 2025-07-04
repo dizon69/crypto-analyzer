a24528262a78aec568d73efde50ea890fa68f0db
dizon69 <dizonfirst@gmail.com>
2025-07-05 02:31:36 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import VolumePage from "./pages/VolumePage";
 import VolatilitasPage from "./pages/VolatilitasPage";
